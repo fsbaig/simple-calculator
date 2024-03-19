d8aca4b57b66b45657fafb764d27b8c4074e4a60
furqan salee <160034257+fsbaig@users.noreply.github.com>
2024-03-20 00:08:35 +0500

    push my calculator to github

---
+++ main.js
@@ -0,0 +1,24 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "enter your first number", type: "number", name: "firstnumber" },
+    { message: "enter your second number", type: "number", name: "secondnumber" },
+    {
+        message: "select operator to perform operators operation",
+        type: "list",
+        name: "operator",
+        choices: ["addition", "subtraction", "multipication", "divide"]
+    },
+]);
+if (answer.operator === "addition") {
+    console.log(answer.firstnumber + answer.secondnumber);
+}
+else if (answer.operator === "subtraction") {
+    console.log(answer.firstnumber - answer.secondnumber);
+}
+else if (answer.operator === "multipication") {
+    console.log(answer.firstnumber * answer.secondnumber);
+}
+else if (answer.operator === "divide") {
+    console.log(answer.firstnumber / answer.secondnumber);
+}
